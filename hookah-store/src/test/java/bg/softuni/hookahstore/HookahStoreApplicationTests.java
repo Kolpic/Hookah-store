e5a970fe87f7e900dd7af9daed2372a1630074a5
Galin Petrov <galincho112@gmail.com>
2023-03-08 14:20:16 +0200

    Added index page and maven dep

---
+++ hookah-store/src/test/java/bg/softuni/hookahstore/HookahStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.hookahstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HookahStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
