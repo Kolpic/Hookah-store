e1b6d5cb46e659794c9098417893165e59c662af
Kolpic <galincho112@gmail.com>
2023-04-12 18:59:58 +0300

    Added by mac

--- hookah-store/src/main/java/bg/softuni/hookahstore/HookahStoreApplication.java
+++ hookah-store/src/main/java/bg/softuni/hookahstore/HookahStoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HookahStoreApplication {
 
+	// Added row on mac !!!
 	public static void main(String[] args) {
 		SpringApplication.run(HookahStoreApplication.class, args);
 	}
